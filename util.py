49ff86d3aebdc8dc97c520dde21f366a92753e85
Ivan Gromakovsky <gie1994@gmail.com>
2016-01-26 16:56:57 +0300

    Fix comment

--- util.py
+++ util.py
@@ -2,6 +2,6 @@ import os
 import tempfile
 
 
-# by default it creates file in /tmp which is relatively small
+# By default TemporaryFile creates file in /tmp which is relatively small
 def tmp_file(**kwargs):
     return tempfile.TemporaryFile(dir=os.getcwd(), **kwargs)
